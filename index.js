c7ea83114f608ba44d1c507ae550ea5f67272f2e
Mangaliso <mangaliso.twala@gmail.com>
2023-07-18 23:51:58 +0200

    show and hide snackbar

---
+++ index.js
@@ -0,0 +1,40 @@
+const btn = document.getElementById("btn");
+const snackbar = document.getElementById("snackbar-container");
+
+const message = "Success";
+let isActive = true;
+
+function createNotigfication(){
+    
+    checkActiveNotification();
+
+}
+
+function checkActiveNotification(){
+
+    
+    if(isActive){
+
+        isActive = false;
+
+        const notification = document.createElement("div");
+        notification.classList.add("snackbar");
+        notification.classList.add("active-snackbar");
+        notification.innerText = message;
+        snackbar.appendChild(notification);
+
+        // remove notification after 5 seconds
+        setTimeout(() => {
+            notification.remove();
+            isActive = true;
+            
+        }, 5000);
+
+    } 
+
+    
+}
+
+
+btn.addEventListener("click", () => createNotigfication());
+
